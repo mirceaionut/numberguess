6c12048cb819ff9e5dc7beb219453607cb3adb50
Ionut <Ionut@DESKTOP-SH1LL9N>
2018-12-14 19:14:06 +0200

    up

---
+++ ConsoleApp3/number try.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int guess = 0;
+            string welcome = "Ghiceste un numar intre 1 si 100";
+            int num = rand.Next(1, 100);
+            Console.WriteLine(welcome);
+
+
+            int i = 0;
+
+            while (guess != num)
+            {
+                try
+                {
+                    guess = Convert.ToInt32(Console.ReadLine());
+
+                    if (guess > num)
+                    {
+                        Console.WriteLine("Prea mare");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Prea mic");
+                    }
+                }
+
+                catch
+                {
+                    Console.WriteLine("Trebuie sa alegi un numar");
+                    i--;
+                }
+
+
+                i++;
+            }
+            Console.WriteLine("Felicitari, ti-au trebuit" + i + " incercari");
+            Console.ReadLine();
+        }
+    }
+}
